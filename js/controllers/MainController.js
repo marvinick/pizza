7db90db6756a2258957d9143cd15e31bbbc3168b
marvin <marvkipi@gmail.com>
2015-12-08 12:39:53 -0800

    pizza init

---
+++ js/controllers/MainController.js
@@ -0,0 +1,38 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: "Bruschetta",
+      description:"Grilled bread garlic, tomatoes, olive oil",
+      price: 4.95
+    }
+  ];
+  $scope.mains = [
+    {
+      name: "Cheese Sticks",
+      description: "Grilled bread garlic, tomatoes, olive oil",
+      price : 10.95
+    },
+    {
+      name: "Cheese Sticks",
+      description: "Grilled bread garlic, tomatoes, olive oil",
+      price : 10.95,
+    },
+    {
+      name: "Cheese Sticks",
+      description: "Grilled bread garlic, tomatoes, olive oil",
+      price : 10.95
+    }
+   ];
+}]);
